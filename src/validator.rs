67d55f8c850df9fbf83a6c3e74b54c437a679755
xm0onh <saeedyazdinezhad@gmail.com>
2024-11-06 23:22:52 -0800

    Add hash chain to the validator

--- src/validator.rs
+++ src/validator.rs
@@ -1,16 +1,18 @@
 use crate::account::Account;
 use crate::transaction::Transaction;
-
+use crate::hashchain::HashChain;
 pub struct Validator {
     pub accounts: Vec<Account>,
+    // TODO: change to a circular linked list
+    pub hash_chain: Vec<HashChain>,
 }
-
 pub const MIN_STAKE: f64 = 100.0;
 
 impl Validator {
     pub fn new() -> Self {
         Self {
             accounts: vec![],
+            hash_chain: vec![HashChain::new()],
         }
     }
 
